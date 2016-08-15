b469046bd5ea001c8ffb4138ba827ef490e36c2b
Samuel Reque <Samuel.Reque@jalasoft.com>
2016-08-15 10:26:41 -0400

    Ordered Posts by timestamp

--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -7,7 +7,7 @@ Posts = new Mongo.Collection('posts');
 });*/
 Meteor.methods({
 	postInsert: function(postAttributes) {
-		check(Meteor.userId(), String);
+		check(this.userId, String);
 		check(postAttributes, {
 			title: String,
 			url: String
