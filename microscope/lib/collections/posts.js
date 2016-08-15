bdd302995321fe75a67285ed676965330715cf01
Samuel Reque <Samuel.Reque@jalasoft.com>
2016-08-15 11:51:46 -0400

    Allow changes only in certain fields

--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -1,10 +1,21 @@
 Posts = new Mongo.Collection('posts');
-/*Posts.allow({
-	insert: function(userId, doc) {
+Posts.allow({
+	/*insert: function(userId, doc) {
 		//only allow posting if you are logged in
 		return !! userId;
+	}*/
+	update: function(userId, post) { return ownsDocument(userId, post);},
+	remove: function(userId, post) { return ownsDocument(userId, post);},
+
+});
+
+Posts.deny({
+	update: function(userId, post, fieldNames) {
+		// may only edit the following two fields:
+		return (_.without(fieldNames, 'url', 'title').length > 0);
 	}
-});*/
+});
+
 Meteor.methods({
 	postInsert: function(postAttributes) {
 		check(this.userId, String);
