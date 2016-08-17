06881de4b9c8b98700e6a010bf1ce2880e0c9a14
Samuel Reque <Samuel.Reque@jalasoft.com>
2016-08-17 16:17:01 -0400

    Fade items in when they are drawn

--- microscope/client/templates/posts/posts_list.js
+++ microscope/client/templates/posts/posts_list.js
@@ -5,6 +5,12 @@
 });*/
 Template.postsList.onRendered(function() {
 	this.find('.wrapper')._uihooks = {
+		insertElement: function (node, next) {
+			$(node)
+				.hide()
+				.insertBefore(next)
+				.fadeIn();
+		},
 		moveElement: function(node, next) {
 			var $node = $(node), $next = $(next);
 			var oldTop = $node.offset().top;
@@ -37,6 +43,11 @@ Template.postsList.onRendered(function() {
 			//reset everything go to 0, animated
 			$node.addClass('animate').css('top', 0);
 			$inBetween.addClass('animate').css('top',0);
+		},
+		removeElement: function (node) {
+			$(node).fadeOut(function() {
+				$(this).remove();
+			});
 		}
 	}
 });
